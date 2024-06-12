2916cc013fbb39b4e37c6baa1c0b3de9b07e1c35
ndiazr <nelsondiazrojas0130@gmail.com>
2024-06-12 15:29:32 -0500

    Add backend project

---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+const generateId = () => {
+  const maxId = persons.length > 0
+  ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.get('/info', (request, response) => {
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+  `)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(p => p.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const payload = request.body
+  
+  if (!payload.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+  if (!payload.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+  if (persons.some(p => p.name === payload.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    ...payload
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
